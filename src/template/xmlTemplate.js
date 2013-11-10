781822e848eb077a119900ba30b31bc94421a5aa
ShaneGH <shane.connon@gmail.com>
2013-11-10 11:11:48 +0000

    cleanup

--- src/template/xmlTemplate.js
+++ src/template/xmlTemplate.js
@@ -4,9 +4,7 @@ wpfko.template = wpfko.template || {};
 
 (function () {
     
-    var _xmlTemplate = function(xmlTemplate) {
-        
-        this._builders = [];
+    var xmlTemplate = function(xmlTemplate) {
                 
         xmlTemplate = new DOMParser().parseFromString("<root>" + xmlTemplate + "</root>", "application/xml").documentElement;
         
@@ -14,11 +12,11 @@ wpfko.template = wpfko.template || {};
         this.htmlBuilder = new wpfko.template.htmlBuilder(xmlTemplate);
     }
     
-    _xmlTemplate.isCustomElement = function(xmlElement) {
+    xmlTemplate.isCustomElement = function(xmlElement) {
         return xmlElement.nodeType == 1 && wpfko.base.visual.reservedTags.indexOf(xmlElement.nodeName.toLowerCase()) === -1;
     };
     
-    _xmlTemplate.getId = function(xmlElement) {
+    xmlTemplate.getId = function(xmlElement) {
         for(var i = 0, ii = xmlElement.attributes.length; i < ii; i++) {
             if(xmlElement.attributes[i].nodeName === "id") {
                 return xmlElement.attributes[i].value;
@@ -28,15 +26,15 @@ wpfko.template = wpfko.template || {};
         return null;
     };
     
-    _xmlTemplate.prototype.render = function(bindingContext) {
+    xmlTemplate.prototype.render = function(bindingContext) {
         var html = this.htmlBuilder.render(bindingContext);
         this.viewModelBuilder.addReferencedElements(bindingContext.$data, html);
         return html;
     };
     
-    _xmlTemplate.prototype.rebuild = function(subject) {
+    xmlTemplate.prototype.rebuild = function(subject) {
         this.viewModelBuilder.rebuild(subject);
     };
     
-    wpfko.template.xmlTemplate = _xmlTemplate;
+    wpfko.template.xmlTemplate = xmlTemplate;
 })();
