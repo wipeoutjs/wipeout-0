7e0c339c8773b7d57380cabe2d25b0ff31b9d998
ShaneGH <shane.connon@gmail.com>
2014-01-18 14:16:27 +0000

    className

--- src/base/visual.js
+++ src/base/visual.js
@@ -20,6 +20,10 @@ Class("wpfko.base.visual", function () {
         
         //The template of the visual, giving it an appearance
         this.templateId = ko.observable(templateId || visual.getDefaultTemplateId());
+        
+        if(DEBUG) {
+            this.className = ko.observable();
+        }
     });
         
     visual.prototype.unRender = function() {
