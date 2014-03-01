1ebf0a0a84908b6ce8fb695b363c929b45c6d328
ShaneGH <shane.connon@gmail.com>
2014-03-01 16:56:09 +0000

    comments

--- src/base/routedEventModel.js
+++ src/base/routedEventModel.js
@@ -11,6 +11,7 @@ Class("wipeout.base.routedEventModel", function () {
     routedEventModel.prototype.triggerRoutedEvent = function(routedEvent, eventArgs) {
         ///<summary>Trigger a routed event which will propogate to any view models where this object is it's model and continue to bubble from there</summary>
         
+        // Used by wo.model to acertain when a routed event should be fired
         this.__triggerRoutedEventOnVM.trigger({routedEvent: routedEvent, eventArgs: eventArgs});
     };;
     
