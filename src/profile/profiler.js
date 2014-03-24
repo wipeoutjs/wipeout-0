a58bfd10210342fd169ff57e8650fdad082f0260
ShaneGH <shane.connon@gmail.com>
2014-03-24 17:35:15 +0000

    profiler oo

--- src/profile/profiler.js
+++ src/profile/profiler.js
@@ -4,41 +4,77 @@ Class("wipeout.profile.profiler", function () {
             throw "This debug tool requires jQuery";
     };
     
-    return {
-        profile: function(rootViewModel) {
-            jQ();
-            var $ = window.jQuery;
-            var profilerKey = "wipeoutprofiler";
-            try {
-                var vms = wipeout.debug.renderedItems(rootViewModel.__woBag.rootHtmlElement);
-                for(var i = 0, ii = vms.length; i < ii; i++)
-                    $(vms[i]).data(profilerKey, {id: i});
+    var profiler = wipeout.base.object.extend(function(vm) {
+        jQ();
+        this.vm = vm;
+        this.cssClass = "wipeout-profiler-" + profiler.newId();
+    });
+    
+    profiler.createStyle = function() {
+        if(profiler.style)
+            return;
+        
+        profiler.style = document.createElement("style");     
+        $("body").append(profiler.style);   
+    };
+    
+    profiler.prototype.level1Nodes = function() {
+        // TODO: where nodeType = 1
+        return wipeout.utils.ko.virtualElements.childNodes(this.vm.__woBag.rootHtmlElement);
+    };
+    
+    var eventNamespace = ".wipeoutProfiler";
+    profiler.prototype.profile = function() {
+        profiler.createStyle();
+        
+        var vm = this.vm;
+        
+        enumerate(this.level1Nodes(), function(element) {
+            $el = $(element);
+            
+            $el.addClass(this.cssClass);
+            $el.on("click" + eventNamespace, function (e) {
+                e.preventDefault();
+                e.stopPropagation();
 
-                var styles = [];
-
-                enumerate(vms, function(vm) {
-                    var cssClass = "wipeout-profiler-" + $(vm).data("wipeoutprofiler").id;
-                    $(wipeout.utils.ko.virtualElements.childNodes(vm.__woBag.rootHtmlElement)).addClass(cssClass);
-
-                    $("." + cssClass).on("click", function (e) {
-                        e.preventDefault();
-                        e.stopPropagation();
-
-                        wipeout.profile.utils.popup(wipeout.profile.utils.generateInfo(vm));
-                    });
-
-                    styles.push("." + cssClass + " {background-color:#" + wipeout.profile.utils.generateColour() + " !important;}");
-                });         
-
-                var style = $("<style>" + styles.join("\n") + "</style>");
-
-                $("body").append(style[0]);
-            } finally {                
-                // cleanup
-                enumerate(vms, function(vm) {
-                    $.removeData(vm, profilerKey);
+                wipeout.profile.utils.popup(wipeout.profile.utils.generateInfo(vm));
+            });            
+        }, this);
+        
+        profiler.style.innerHTML += "." + this.cssClass + " {background-color:#" + wipeout.profile.utils.generateColour() + " !important;}";
+    };
+    
+    profiler.prototype.dispose = function() {
+        // TODO: where nodeType = 1
+        $(this.level1Nodes())
+            .off("click" + eventNamespace)
+            .removeClass(this.cssClass);
+    };
+        
+    profiler.newId = (function () {
+        var i = 0;
+        return function() {
+            return ++i;
+        };
+    })();
+    
+    profiler.profile = function(rootViewModel) {
+        
+        var profiles = [];        
+        enumerate(wipeout.debug.renderedItems(rootViewModel.__woBag.rootHtmlElement), function(vm) {
+            var p = new profiler(vm);
+            p.profile();
+            profiles.push(p);
+        });
+        
+        return {
+            dispose: function() {
+                enumerate(profiles, function(profile) {
+                    profile.dispose();
                 });
             }
         }
-    };    
+    };
+    
+    return profiler;
 });
