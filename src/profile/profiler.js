ecc6962b4aa124a816c2d0094f479cc8f99a2714
ShaneGH <shane.connon@gmail.com>
2014-03-26 18:34:24 +0000

    profiler

--- src/profile/profiler.js
+++ src/profile/profiler.js
@@ -41,7 +41,8 @@ Class("wipeout.profile.profiler", function () {
             });            
         }, this);
         
-        profiler.style.innerHTML += "." + this.cssClass + " {background-color:#" + wipeout.profile.utils.generateColour() + " !important;}";
+        //profiler.style.innerHTML += "." + this.cssClass + " {background-color:#" + wipeout.profile.utils.generateColour() + " !important;}";
+        profiler.style.innerHTML += "." + this.cssClass + " {background-color:#efcdd9 !important;}";
     };
     
     profiler.prototype.dispose = function() {
@@ -58,22 +59,52 @@ Class("wipeout.profile.profiler", function () {
         };
     })();
     
-    profiler.profile = function(rootViewModel) {
+    function queue() {
+        this.items = [];
+    };
+    
+    queue.prototype.push = function(callback) {
+        var _this = this;
+        var cb = function() {
+            
+            _this.items.splice(_this.items.indexOf(cb), 1);
+            
+            callback();
+                        
+            if (_this.items.length)
+                _this.items.splice(0, 1)[0](_this);
+        };
         
-        var profiles = [];        
-        enumerate(wipeout.debug.renderedItems(rootViewModel.__woBag.rootHtmlElement), function(vm) {
-            var p = new profiler(vm);
-            p.profile();
-            profiles.push(p);
-        });
+        this.items.push(cb);
         
-        return {
-            dispose: function() {
-                enumerate(profiles, function(profile) {
-                    profile.dispose();
+        if(this.items[0] === cb)
+            cb();
+    };
+    
+    profiler.profile = function() {
+        var timeout;
+        var q = new queue();
+        var currentElement = null;
+        var currentProfiler = {dispose: function(){}};
+        $("body").on("mouseover", function(e) {
+            if(!e.fromElement || currentElement === e.fromElement) return;
+            
+            currentElement = e.fromElement;
+            var vm = wipeout.utils.html.getViewModel(currentElement);
+            if(!vm || vm === currentProfiler.vm) return;
+                        
+            if(timeout) clearTimeout(timeout);
+            
+            // makes it less jumpy
+            timeout = setTimeout(function() {
+                // like thread.lock
+                q.push(function() {
+                    currentProfiler.dispose();
+                    currentProfiler = new profiler(vm);
+                    currentProfiler.profile();
                 });
-            }
-        }
+            }, 100);
+        });
     };
     
     return profiler;
