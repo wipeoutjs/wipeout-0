1ba27340e15ec93ef6c8b565aa298e25cc5c0303
ShaneGH <shane.connon@gmail.com>
2014-04-13 15:17:19 +0100

    x

--- scripts/app/Models/Descriptions/Argument.js
+++ scripts/app/Models/Descriptions/Argument.js
@@ -1,5 +1,5 @@
 compiler.registerClass("Wipeout.Docs.Models.Descriptions.Argument", "wo.object", function() {
-    return function(itemDetails) {
+    function argument(itemDetails) {
         this._super();
         
         this.name = itemDetails.name;
@@ -7,4 +7,20 @@ compiler.registerClass("Wipeout.Docs.Models.Descriptions.Argument", "wo.object",
         this.optional = !!itemDetails.optional;
         this.description = itemDetails.description;
     }
+    
+    argument.prototype.split = function() {
+        var output = [];        
+        wo.obj.enumerate(this.type.split(","), function(type) {
+            output.push(new Wipeout.Docs.Models.Descriptions.Argument({
+                name: this.name,
+                type: type,
+                optional: this.optional,
+                description: this.description
+            }));
+        });
+        
+        return output;        
+    };
+    
+    return argument;
 });
