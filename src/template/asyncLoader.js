f5f2a30ef5801b550c41893d9b989ea3592b680f
ShaneGH <shane.connon@gmail.com>
2014-05-16 18:33:43 +0100

    comments

--- src/template/asyncLoader.js
+++ src/template/asyncLoader.js
@@ -1,14 +1,19 @@
 Class("wipeout.template.asyncLoader", function () {
     
     var asyncLoader = function() {
+        ///<summary>Loads remote templates and runs callbacks when the template is added to the DOM</summary>
+        
         this.pending = {};
     };
     
     asyncLoader.prototype.load = function(templateId, success) {
+        ///<summary>Load a template</summary>
+        ///<param name="templateId" type="string" optional="false">The url for the template to load. This will also be the id when the template is loaded</param>
+        ///<param name="success" type="Function" optional="true">Run when the template exists in the DOM</param>
         if (!this.pending[templateId])
             this.pending[templateId] = new loader(templateId);
             
-        this.pending[templateId].add(success);
+        this.pending[templateId].add(success || function() {});
     };
     
     var loader = function(templateName) {
@@ -19,7 +24,9 @@ Class("wipeout.template.asyncLoader", function () {
         ajax({
             type: "GET",
             url: templateName,
-            success: function(result) {
+            success: function(result) {                
+                wipeout.base.contentControl.createTemplate(templateName, result.responseText);                
+                
                 _this.success = true;
                 var callbacks = _this._callbacks;
                 delete _this._callbacks;
@@ -40,7 +47,7 @@ Class("wipeout.template.asyncLoader", function () {
             this.callbacks.push(success);
         else if(this.success)
             success();
-        else
+        else // success is null or false
             throw "Could not locate template \"" + templateName + "\"";
     }
     
