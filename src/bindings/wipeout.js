b2c0e0a4510866da824ee7806178ed5bbe189bd6
ShaneGH <shane.connon@gmail.com>
2014-05-23 16:22:01 +0100

    ooed wipeout binding

--- src/bindings/wipeout.js
+++ src/bindings/wipeout.js
@@ -1,45 +1,38 @@
 
 Binding("wipeout", true, function () {
-        
-    var init = function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-        ///<summary>Initialize the wipeout binding. The wipeout binding is the entry point into a wipeout application</summary>
-
-        //TODO: knockout standard way of controling element        
-        //TODO: add optional inline properties to binding   
-        
-        if(wipeout.utils.html.getViewModel(element))
-            throw "This element is already bound to another model";
-        
-        var type = valueAccessor();
-        if(!type)
-            throw "Invalid view type";
-            
-        var view = new type();
-        if(!(view instanceof wipeout.base.view))
-            throw "Invalid view type";        
-        
-        view.model(viewModel);   
-        
-        var output = ko.bindingHandlers.render.init.call(this, element, createValueAccessor(view), allBindingsAccessor, null, bindingContext);
-        
-        view.onApplicationInitialized();
-        
-        return output;
-    };
     
-    var createValueAccessor = function(view) {
-        ///<summary>Create a value accessor for the render binding.</summary>
-        return function() {
-            return view;
-        };
-    };
-     
-    return {
-        init: init,
-        utils: {
-            createValueAccessor: createValueAccessor,
-            //TODO: is this used
-            wipeoutKey: "__wipeout"
+    return wipeout.bindings.bindingBase.extend({
+        constructor: function(element, type, allBindingsAccessor, viewModel, bindingContext) {
+            this._super(element);   
+        
+            if(wipeout.utils.html.getViewModel(element))
+                throw "This element is already bound to another model";
+
+            if(!type || !(type instanceof Function))
+                throw "Invalid view type";
+
+            this.renderedView = new type();
+            if(!(this.renderedView instanceof wipeout.base.view))
+                throw "Invalid view type";  
+            
+            this.renderedView.model(viewModel);            
+            this.render = new wipeout.bindings.render(element, this.renderedView, allBindingsAccessor, bindingContext);
+            this.render.render(this.renderedView);        
+            this.renderedView.onApplicationInitialized();
+        },
+        dispose: function() {
+            this.render.dispose();
+            this.renderedView.dispose();
+            this._super();
+        },
+        statics: {
+            init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+                ///<summary>Initialize the render binding</summary>                
+                new wipeout.bindings.wipeout(element, valueAccessor(), allBindingsAccessor, viewModel, bindingContext);
+            },
+            utils: {
+                wipeoutKey: "__wipeout"
+            }
         }
-    };
+    });
 });
