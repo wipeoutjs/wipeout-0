5f61586e4ddd96ce69a3022cc7f2f19f4726a8b3
ShaneGH <shane.connon@gmail.com>
2014-06-25 19:54:49 +0100

    split out wipeout binding

--- src/bindings/wipeout.js
+++ src/bindings/wipeout.js
@@ -1,39 +1,40 @@
 
 Binding("wipeout", true, function () {
     
-    return wipeout.bindings.render.extend({
-        constructor: function(element, type, allBindingsAccessor, viewModel, bindingContext) {  
+    var _wipeout = wipeout.bindings.render.extend(function(element, type, allBindingsAccessor, viewModel, bindingContext) {  
 
-            if(!(type instanceof Function))
-                throw "Invalid view type";
+        if(!(type instanceof Function))
+            throw "Invalid view type";
 
-            this.renderedView = new type();
-            if(!(this.renderedView instanceof wipeout.base.view))
-                throw "Invalid view type";
-            
-            this._super(element, this.renderedView, allBindingsAccessor, bindingContext);
-            
-            if (this.renderedView.shareParentScope)
-                throw "The root of an application cannot share its parents scope";
-            
-            this.renderedView.model(viewModel);                   
-            this.render(this.renderedView);
-            this.render = function() { throw "Cannont render this binding a second time, use the render binding instead"; };
-            
-            this.renderedView.onApplicationInitialized();
-        },
-        dispose: function() {
-            this._super();
-            this.renderedView.dispose();
-        },
-        statics: {
-            init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-                ///<summary>Initialize the render binding</summary>                
-                return new wipeout.bindings.wipeout(element, valueAccessor(), allBindingsAccessor, viewModel, bindingContext).bindingMeta;
-            },
-            utils: {
-                wipeoutKey: "__wipeout"
-            }
-        }
-    });
+        this.renderedView = new type();
+        if(!(this.renderedView instanceof wipeout.base.view))
+            throw "Invalid view type";
+
+        this._super(element, this.renderedView, allBindingsAccessor, bindingContext);
+
+        if (this.renderedView.shareParentScope)
+            throw "The root of an application cannot share its parents scope";
+
+        this.renderedView.model(viewModel);                   
+        this.render(this.renderedView);
+        this.render = function() { throw "Cannont render this binding a second time, use the render binding instead"; };
+
+        this.renderedView.onApplicationInitialized();
+    }, "wipeout");
+    
+    _wipeout.prototype.dispose = function() {
+        this._super();
+        this.renderedView.dispose();
+    };
+    
+    _wipeout.init = function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        ///<summary>Initialize the render binding</summary>                
+        return new wipeout.bindings.wipeout(element, valueAccessor(), allBindingsAccessor, viewModel, bindingContext).bindingMeta;
+    };
+    
+    _wipeout.utils = {
+        wipeoutKey: "__wipeout"
+    };
+    
+    return _wipeout;
 });
